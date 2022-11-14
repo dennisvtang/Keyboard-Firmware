5a97790ce45d1dd4e8c592a2010506cd365e747e
Dennis Tang <dennisvtang@gmail.com>
2022-11-13 18:51:02 -0800

    Setup outline for script

---
+++ Preonic/build/build_keymap.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def main(qmk_exe_path: str):
+    # todo start up qmk
+    # todo convert json to c
+    # todo copy results
+    # todo overwrite keymap.c
+    # todo build with qmk
+    # todo copy resulting bin file
+
+
+if __name__ == '__main__':
+    qmk_exe_path = 'C:\QMK_MSYS\conemu\ConEmu64.exe'
+    main(qmk_exe_path)
