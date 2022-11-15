0dbf3352a1e4ee669a3f2ddf0402576872f1b0ea
Dennis Tang <dennisvtang@gmail.com>
2022-11-14 19:38:28 -0800

    Add code to run json2c command

--- Preonic/build/build_keymap.py
+++ Preonic/build/build_keymap.py
@@ -3,7 +3,7 @@ from WindowCapture import WindowCapture
 import time
 
 
-def main(qmk_exe_path: str):
+def main(qmk_exe_path: str, keymap_json_path: str, keymap_c_path: str):
     # open qmk and wait for it to start
     results = subprocess.Popen(
         qmk_exe_path,
@@ -13,13 +13,18 @@ def main(qmk_exe_path: str):
 
     # identify qmk terminal
     qmk_window = WindowCapture('bash')
-    # todo convert json to c
-    # todo copy results
-    # todo overwrite keymap.c
+
+    # convert json to c
+    qmk_window.send_command(
+        f'qmk json2c {keymap_json_path} -o {keymap_c_path}'
+    )
+
     # todo build with qmk
     # todo copy resulting bin file
 
 
 if __name__ == '__main__':
     qmk_exe_path = 'C:\QMK_MSYS\conemu\ConEmu64.exe'
-    main(qmk_exe_path)
+    keymap_json_path = '../src/preonic_rev3_qitbit_keymap.json'
+    keymap_c_path = '../src/qitbit/keymap.c'
+    main(qmk_exe_path, keymap_json_path, keymap_c_path)
