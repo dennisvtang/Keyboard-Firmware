e19ff9cb65d2c4335344729e13eda690346ce898
Dennis Tang <dennisvtang@gmail.com>
2022-11-14 19:09:35 -0800

    Identify QMK terminal to send commands to

--- Preonic/build/build_keymap.py
+++ Preonic/build/build_keymap.py
@@ -1,13 +1,18 @@
 import subprocess
+from WindowCapture import WindowCapture
+import time
 
 
 def main(qmk_exe_path: str):
-    # start up qmk
+    # open qmk and wait for it to start
     results = subprocess.Popen(
         qmk_exe_path,
         shell=True,
     )
+    time.sleep(1)
 
+    # identify qmk terminal
+    qmk_window = WindowCapture('bash')
     # todo convert json to c
     # todo copy results
     # todo overwrite keymap.c
