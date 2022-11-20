802eb8cb23947bd95072899c484083b587469bc8
Dennis Tang <dennisvtang@gmail.com>
2022-11-20 13:44:56 -0800

    Refactor function to return instead of directly printing

--- Preonic/build/WindowCapture.py
+++ Preonic/build/WindowCapture.py
@@ -7,17 +7,25 @@ class WindowCapture:
         # find the handle for the window we want to capture
         self.hwnd = win32gui.FindWindow(None, window_name)
         if not self.hwnd:
-            self.list_window_names()
             raise Exception(f'Window not found: {window_name}')
+            window_names = self.get_window_names()
 
     # find the name of the window you're interested in.
     # once you have it, update window_capture()
     # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
-    def list_window_names(self):
-        def winEnumHandler(hwnd, ctx):
+    def get_window_names(self):
+        def winEnumHandler(hwnd, extra_args):
+            # only get windows that are visible
             if win32gui.IsWindowVisible(hwnd):
-                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+                window_names.append(
+                    f'{hex(hwnd)} {win32gui.GetWindowText(hwnd)}'
+                )
+        window_names = []
+
+        # enumerate top-level windows on screen with a callback function
         win32gui.EnumWindows(winEnumHandler, None)
+
+        return window_names
 
     def send_command(self, command, send_enter=True):
         # make window active
