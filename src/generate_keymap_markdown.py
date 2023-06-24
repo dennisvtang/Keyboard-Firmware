800581b3731d15e0097b8be940f230cb2482a4ee
Dennis Tang <dennisvtang@gmail.com>
2023-06-24 00:06:59 -0700

    Add code that splits layers into rows

--- src/generate_keymap_markdown.py
+++ src/generate_keymap_markdown.py
@@ -6,6 +6,19 @@ keymap_path = pathlib.Path(
     'C:/Users/denni/Desktop/Programming/Keyboard-Firmware/Preonic/src/qitbit/keymap.json')
 with open(keymap_path) as file:
     keymap = json.load(file)['layers']
-print(json.dumps(keymap, indent=4, default=str))
 
-# todo convert lists to markdown
+
+max_row_size = 12  # todo read this value from a keymap file
+
+# todo generate corresponding markdown
+for index, layer in enumerate(keymap):
+    organized_layer = []
+
+    # split layer into rows
+    row = []
+    for key in layer:
+        if len(row) == max_row_size:
+            organized_layer.append(row)
+            row = []
+        row.append(key)
+    organized_layer.append(row)
